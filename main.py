1a4063b6ace61902af88197895beed8c37f46fc3
EGO <ego.argote@hotmail.com>
2024-09-13 10:21:09 -0500

    abordamos hasta la sesion 3

--- main.py
+++ main.py
@@ -1,83 +1,10 @@
-from fastapi import FastAPI #importa la libreria
+from fastapi import FastAPI, Body #importa la libreria
 from fastapi.responses import HTMLResponse #importa la libreria
+from movies_list import movies_list #importa la libreria
+
 app = FastAPI() #crea una instancia de la clase FastAPI
 app.title = 'Mi app con FastAPI - Movies' #asigna un valor a la variable title
 app.version = "0.0.1" #asigna un valor a la variable version
-
-movies_list = [
-    
-    {
-        "id": 1,
-        "title": "The Matrix",
-        "overview": "pelicula de ficcion",
-        "year": "1999",
-        "rating": 8.7
-    },
-    {
-        "id": 2,
-        "title": "The Matrix 2",
-        "overview": "pelicula de ficcion 2",
-        "year": "2000",
-        "rating": 8.7
-    },
-    {
-        "id": 3,
-        "title": "The Matrix 3",
-        "overview": "pelicula de ficcion 3",
-        "year": "2001",
-        "rating": 8.7
-    },
-    {
-        "id": 4,
-        "title": "The Matrix 4",
-        "overview": "pelicula de ficcion 4",
-        "year": "2021",
-        "rating": 8.7
-    },
-    {
-        "id": 5,
-        "title": "shreck",
-        "overview": "pelicula de ficcion 5",
-        "year": "2003",
-        "rating": 8.7
-    },
-    {
-        "id": 6,
-        "title": "shreck 2",
-        "overview": "pelicula de ficcion 6",
-        "year": "2004",
-        "rating": 8.7
-    },
-    {
-        "id": 7,
-        "title": "shreck 3",
-        "overview": "pelicula de ficcion 7",
-        "year": "2005",
-        "rating": 8.7
-    },
-    {
-        "id": 8,
-        "title": "shreck para siempre",
-        "overview": "pelicula de ficcion 8",
-        "year": "2006",
-        "rating": 8.7
-    },
-    {
-        "id": 9,
-        "title": "castigador",
-        "overview": "pelicula de ficcion 9",
-        "year": "2007",
-        "rating": 8.7
-    },
-    {
-        "id": 10,
-        "title": "castigador zona de guerra",
-        "overview": "pelicula de ficcion 10",
-        "year": "2008",
-        "rating": 8.7
-    }
-    
-]
 
 @app.get('/', tags=["Home"]) #definimos una ruta
 def message(): #definimos una función
@@ -96,3 +23,24 @@ def get_movies (id:int): #definimos una función
 
 # Cuando no se puede escribir en la terminal se da ctrl+c y el programa se detiene
 
+@app.get ('/movies/', tags=["Movies"])
+def get_movies_by_category (category: str, year: int): 
+    return [item for item in movies_list if item["year"] == year]
+
+@app.post ('/movies/', tags=["Movies"])
+def create_movie (
+    id: int = Body(), 
+    title: str = Body(), 
+    overview: str = Body(), 
+    year: int = Body(), 
+    rating: float = Body(), 
+    category: str = Body()):
+    movies_list.append ({
+        "id": id, 
+        "title": title, 
+        "overview": overview, 
+        "year": year, 
+        "rating": rating, 
+        "category": category
+        })
+    return movies_list
---
+++ movies_list.py
@@ -0,0 +1,83 @@
+movies_list = [
+    {
+        "id": 1,
+        "title": "The Matrix",
+        "overview": "pelicula de ficcion",
+        "year": 1999,
+        "rating": 8.7,
+        "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "The Matrix 2",
+        "overview": "pelicula de ficcion 2",
+        "year": 2000,
+        "rating": 8.7,
+        "category": "Acción"
+    },
+    {
+        "id": 3,
+        "title": "The Matrix 3",
+        "overview": "pelicula de ficcion 3",
+        "year": 2001,
+        "rating": 8.7,
+        "category": "Acción"
+    },
+    {
+        "id": 4,
+        "title": "The Matrix 4",
+        "overview": "pelicula de ficcion 4",
+        "year": 2021,
+        "rating": 8.7,
+        "category": "Acción"
+    },
+    {
+        "id": 5,
+        "title": "shreck",
+        "overview": "pelicula de ficcion 5",
+        "year": 2003,
+        "rating": 8.7,
+        "category": "Comedia"
+    },
+    {
+        "id": 6,
+        "title": "shreck 2",
+        "overview": "pelicula de ficcion 6",
+        "year": 2004,
+        "rating": 8.7,
+        "category": "Comedia"
+    },
+    {
+        "id": 7,
+        "title": "shreck 3",
+        "overview": "pelicula de ficcion 7",
+        "year": 2005,
+        "rating": 8.7,
+        "category": "Comedia"
+    },
+    {
+        "id": 8,
+        "title": "shreck para siempre",
+        "overview": "pelicula de ficcion 8",
+        "year": 2006,
+        "rating": 8.7,
+        "category": "Comedia"
+    },
+    {
+        "id": 9,
+        "title": "castigador",
+        "overview": "pelicula de ficcion 9",
+        "year": 2007,
+        "rating": 8.7,
+        "category": "Acción"
+    },
+    {
+        "id": 10,
+        "title": "castigador zona de guerra",
+        "overview": "pelicula de ficcion 10",
+        "year": 2008,
+        "rating": 8.7,
+        "category": "Acción"
+    }
+    
+]
